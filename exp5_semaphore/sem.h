7332e606f9a36ad381c7cd3f862971d2b24021ce
Wangzhike <956318206@qq.com>
2015-07-14 19:33:27 +0800

    sem.h

---
+++ exp5_semaphore/sem.h
@@ -0,0 +1,16 @@
+#ifndef _SEM_H
+#define _SEM_H
+
+#include <linux/sched.h>
+
+#define SEMTABLE_LEN	20
+#define SEM_NAME_LEN    20
+
+typedef struct semaphore{
+    char name[SEM_NAME_LEN];
+    int value;
+    struct task_struct *queue;
+} sem_t;
+extern sem_t semtable[SEMTABLE_LEN];
+
+#endif
