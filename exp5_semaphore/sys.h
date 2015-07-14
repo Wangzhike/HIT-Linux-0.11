a5a0f75cda03016ded940721a36e6cc05654cdac
Wangzhike <956318206@qq.com>
2015-07-14 19:57:25 +0800

    linux-0.11/include/linux/sys.h

--- exp5_semaphore/sys.h
+++ exp5_semaphore/sys.h
@@ -75,6 +75,7 @@ extern int sys_sem_wait();
 extern int sys_sem_post();
 extern int sys_sem_unlink();
 
+
 fn_ptr sys_call_table[] = { sys_setup, sys_exit, sys_fork, sys_read,
 sys_write, sys_open, sys_close, sys_waitpid, sys_creat, sys_link,
 sys_unlink, sys_execve, sys_chdir, sys_time, sys_mknod, sys_chmod,
